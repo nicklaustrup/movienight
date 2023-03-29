df7ad9738cf61fb2a81ade89a617779cede95712
Eduarddo Angulo <eanguloe@gmail.com>
2023-03-29 10:26:40 -0700

    Restore Frontend

--- Frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {retrieveUseName()};
-function retrieveUseName() {
-    var userName = window.localStorage.getItem('userName'); //searches for the user name in localStorage
-    document.getElementById("userName").innerHTML = `User Name is "` + userName + `"`;
-}
