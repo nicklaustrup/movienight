972d432a8c0b1e354f019824b94c2e2c5948dd77
hahaufunnay <malik95hamza@gmail.com>
2023-04-02 17:34:47 -0700

    Added unit tests for service class + added find all user methods to required classes

--- Application/src/main/java/com/kenzie/appserver/controller/UserController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/UserController.java
@@ -1,17 +1,15 @@
 package com.kenzie.appserver.controller;
 
-import com.kenzie.appserver.controller.model.ExampleCreateRequest;
-import com.kenzie.appserver.controller.model.ExampleResponse;
-import com.kenzie.appserver.controller.model.UserCreateRequest;
-import com.kenzie.appserver.controller.model.UserResponse;
-import com.kenzie.appserver.service.ExampleService;
+import com.kenzie.appserver.controller.model.*;
+import com.kenzie.appserver.repositories.model.UserRecord;
 import com.kenzie.appserver.service.UserService;
-import com.kenzie.appserver.service.model.Example;
 import com.kenzie.appserver.service.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.util.UUID.randomUUID;
 
@@ -56,4 +54,27 @@ public class UserController {
 
         return ResponseEntity.created(URI.create("/user/" + userResponse.getUserId())).body(userResponse);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+
+        List<UserRecord> allUsers = userService.findAll();
+
+        if (allUsers.size() == 0) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<UserResponse> allUserResponse = new ArrayList<>();
+
+        for (UserRecord user: allUsers) {
+            UserResponse userResponse = new UserResponse();
+            userResponse.setUserId(user.getUserId());
+            userResponse.setFirstName(user.getFirstName());
+            userResponse.setLastName(user.getLastName());
+            allUserResponse.add(userResponse);
+        }
+
+        return ResponseEntity.ok(allUserResponse);
+    }
 }
+
