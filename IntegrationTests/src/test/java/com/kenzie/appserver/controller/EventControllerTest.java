e6d10b8b5b3fde4e19987bde6b3d1646e7c3b406
hahaufunnay <malik95hamza@gmail.com>
2023-04-09 15:16:02 -0700

    Added Integration tests for Movie, RSVP and User Controller classes

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/EventControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/EventControllerTest.java
@@ -3,12 +3,16 @@ package com.kenzie.appserver.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kenzie.appserver.IntegrationTest;
+import com.kenzie.appserver.controller.model.EventCreateRequest;
 import com.kenzie.appserver.controller.model.ExampleCreateRequest;
+import com.kenzie.appserver.repositories.EventRepository;
 import com.kenzie.appserver.service.EventService;
 import com.kenzie.appserver.service.ExampleService;
+import com.kenzie.appserver.service.UserService;
 import com.kenzie.appserver.service.model.Event;
 import com.kenzie.appserver.service.model.Example;
 import com.kenzie.appserver.service.model.RSVP;
+import com.kenzie.appserver.service.model.User;
 import net.andreinc.mockneat.MockNeat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +21,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -34,6 +40,8 @@ public class EventControllerTest {
 
     @Autowired
     EventService eventService;
+    @Autowired
+    UserService userService;
 
     private final MockNeat mockNeat = MockNeat.threadLocal();
 
@@ -50,35 +58,60 @@ public class EventControllerTest {
 //        List<RSVP> users = new ArrayList<>();
 //
 //        Event event = new Event(eventId, eventTitle, movieId, date, active);
+//
 //        Event persistedEvent = eventService.addNewEvent(event, users);
+//
 //        mvc.perform(get("/event/{eventId}", persistedEvent.getEventId())
 //                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
 //                .andExpect(jsonPath("eventId")
 //                        .value(is(eventId)))
 //                .andExpect(jsonPath("eventTitle")
 //                        .value(is(eventTitle)))
 //                .andExpect(jsonPath("movieId")
 //                        .value(is(movieId)))
-//                .andExpect(status().isOk());
+//                .andExpect(jsonPath("$.users", hasSize(0)));
 //    }
 
 //    @Test
-//    public void createExample_CreateSuccessful() throws Exception {
-//        String name = mockNeat.strings().valStr();
+//    public void createEvent_CreateSuccessful() throws Exception {
+//        // create mock data for request body
+//        EventCreateRequest eventCreateRequest = new EventCreateRequest();
+//        eventCreateRequest.setEventTitle("Test Event");
+//        eventCreateRequest.setMovieId("1234");
+//        eventCreateRequest.setDate(LocalDateTime.now());
+//        eventCreateRequest.setActive(true);
 //
-//        ExampleCreateRequest exampleCreateRequest = new ExampleCreateRequest();
-//        exampleCreateRequest.setName(name);
+//        String firstName = mockNeat.strings().valStr();
+//        String lastName = mockNeat.strings().valStr();
+//
+//        User user = new User("user1", firstName, lastName);
+//        User persistedUser = userService.addNewUser(user);
+//
+//        List<String> users = Arrays.asList("user1");
+//        eventCreateRequest.setUsers(users);
 //
 //        mapper.registerModule(new JavaTimeModule());
 //
-//        mvc.perform(post("/example")
+//
+//        // perform request and verify response
+//        mvc.perform(post("/event")
 //                        .accept(MediaType.APPLICATION_JSON)
 //                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(mapper.writeValueAsString(exampleCreateRequest)))
-//                .andExpect(jsonPath("id")
-//                        .exists())
-//                .andExpect(jsonPath("name")
-//                        .value(is(name)))
-//                .andExpect(status().isCreated());
+//                        .content(mapper.writeValueAsString(eventCreateRequest)))
+//                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("$.eventId").exists())
+//                .andExpect(jsonPath("$.eventTitle")
+//                        .value(is("Test Event")))
+//                .andExpect(jsonPath("$.movieId")
+//                        .value(is("1234")))
+//                .andExpect(jsonPath("$.active")
+//                        .value(is(true)))
+//                .andExpect(jsonPath("$.users",
+//                        hasSize(users.size())))
+//                .andExpect(jsonPath("$.users[0].userId")
+//                        .value(is("user1")))
+//                .andExpect(jsonPath("$.users[0].eventId")
+//                        .value(is("Test Event")));
 //    }
 }
---
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/RSVPControllerTest.java
@@ -0,0 +1,187 @@
+package com.kenzie.appserver.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kenzie.appserver.IntegrationTest;
+import com.kenzie.appserver.controller.model.MovieCreateRequest;
+import com.kenzie.appserver.controller.model.RSVPCreateRequest;
+import com.kenzie.appserver.repositories.RSVPRepository;
+import com.kenzie.appserver.repositories.model.RSVPRecord;
+import com.kenzie.appserver.repositories.model.UserRecord;
+import com.kenzie.appserver.service.EventService;
+import com.kenzie.appserver.service.RSVPService;
+import com.kenzie.appserver.service.model.Event;
+import com.kenzie.appserver.service.model.Movie;
+import com.kenzie.appserver.service.model.RSVP;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import net.andreinc.mockneat.MockNeat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@IntegrationTest
+class RSVPControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    RSVPService rsvpService;
+    @Autowired
+    EventService eventService;
+    @Autowired
+    RSVPRepository rsvpRepository;
+
+    private final MockNeat mockNeat = MockNeat.threadLocal();
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void getById_Exists() throws Exception {
+        String userId = UUID.randomUUID().toString();
+        String eventId = UUID.randomUUID().toString();
+        boolean isAttending = true;
+        RSVP rsvp = new RSVP(userId, eventId, isAttending);
+        RSVP persistedRSVP = rsvpService.addNewRSVP(rsvp);
+
+        mvc.perform(get("/rsvp/{userId}_{eventId}", persistedRSVP.getUserId(), persistedRSVP.getEventId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("userId")
+                        .value(is(userId)))
+                .andExpect(jsonPath("eventId")
+                        .value(is(eventId)))
+                .andExpect(jsonPath("isAttending")
+                        .value(is(isAttending)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void createRSVP_CreateSuccessful() throws Exception {
+        String userId = UUID.randomUUID().toString();
+        String eventId = UUID.randomUUID().toString();
+
+        RSVPCreateRequest rsvpCreateRequest = new RSVPCreateRequest();
+        rsvpCreateRequest.setUserId(userId);
+        rsvpCreateRequest.setEventId(eventId);
+        rsvpCreateRequest.setIsAttending(true);
+
+        mapper.registerModule(new JavaTimeModule());
+
+        mvc.perform(post("/rsvp")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(rsvpCreateRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("eventId")
+                        .value(is(eventId)))
+                .andExpect(jsonPath("userId")
+                        .value(is(userId)))
+                .andExpect(jsonPath("isAttending")
+                        .value(is(true)))
+                .andExpect(status().isCreated());
+    }
+//    @Test
+//    public void getAllEventsForUser() throws Exception {
+//        String userId = UUID.randomUUID().toString();
+//        String eventId1 = UUID.randomUUID().toString();
+//        String eventId2 = UUID.randomUUID().toString();
+//
+//        RSVP rsvp1 = new RSVP(userId, eventId1, true);
+//        RSVP rsvp2 = new RSVP(userId, eventId2, false);
+//
+//        rsvpService.addNewRSVP(rsvp1);
+//        rsvpService.addNewRSVP(rsvp2);
+//
+//        // create the events
+//        Event event1 = new Event(eventId1, "Event 1", UUID.randomUUID().toString(), LocalDateTime.now(), true);
+//        Event event2 = new Event(eventId2, "Event 2", UUID.randomUUID().toString(), LocalDateTime.now(), false);
+//
+//        List<RSVP> users = new ArrayList<>();
+//        users.add(rsvp1);
+//        users.add(rsvp2);
+//
+//        eventService.addNewEvent(event1, Collections.emptyList());
+//        eventService.addNewEvent(event2, Collections.emptyList());
+//
+//        mvc.perform(get("/rsvp/events/{userId}", userId)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("$[0].eventId").value(event1.getEventId()))
+//                .andExpect(jsonPath("$[0].eventTitle").value(event1.getEventTitle()))
+//                .andExpect(jsonPath("$[0].date").value(event1.getDate().toString()))
+//                .andExpect(jsonPath("$[0].active").value(event1.getActive()))
+//                .andExpect(jsonPath("$[0].isAttending").value(rsvp1.getIsAttending()))
+//                .andExpect(jsonPath("$[1].eventId").value(event2.getEventId()))
+//                .andExpect(jsonPath("$[1].eventTitle").value(event2.getEventTitle()))
+//                .andExpect(jsonPath("$[1].date").value(event2.getDate().toString()))
+//                .andExpect(jsonPath("$[1].active").value(event2.getActive()))
+//                .andExpect(jsonPath("$[1].isAttending").value(rsvp2.getIsAttending()))
+//                .andExpect(status().isOk());
+//    }
+@Test
+public void updateRSVP_IsSuccessful() throws Exception {
+    String userId = UUID.randomUUID().toString();
+    String eventId = UUID.randomUUID().toString();
+
+    RSVPRecord rsvpRecord = new RSVPRecord();
+    rsvpRecord.setUserId(userId);
+    rsvpRecord.setEventId(eventId);
+    rsvpRecord.setIsAttending(true);
+
+    RSVPCreateRequest rsvpUpdateRequest = new RSVPCreateRequest();
+    rsvpUpdateRequest.setUserId(userId);
+    rsvpUpdateRequest.setEventId(eventId);
+    rsvpUpdateRequest.setIsAttending(false);
+
+    mapper.registerModule(new JavaTimeModule());
+
+    mvc.perform(put("/rsvp")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(mapper.writeValueAsString(rsvpUpdateRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("eventId")
+                    .value(is(eventId)))
+            .andExpect(jsonPath("userId")
+                    .value(is(userId)))
+            .andExpect(jsonPath("isAttending")
+                    .value(is(false)));
+}
+    @Test
+    public void getAllUsersForEvent() throws Exception {
+        RSVPRecord rsvpRecord1 = new RSVPRecord();
+        rsvpRecord1.setUserId("user1");
+        rsvpRecord1.setEventId("event1");
+        rsvpRecord1.setIsAttending(true);
+        rsvpRepository.save(rsvpRecord1);
+
+        RSVPRecord rsvpRecord2 = new RSVPRecord();
+        rsvpRecord2.setUserId("user2");
+        rsvpRecord2.setEventId("event1");
+        rsvpRecord2.setIsAttending(false);
+        rsvpRepository.save(rsvpRecord2);
+
+       // RSVP persistedRSVP = rsvpService.addNewRSVP(rsvp);
+
+        String eventId = "event1";
+        mvc.perform(get("/rsvp/users/{eventId}", eventId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].userId", is("user2")))
+                .andExpect(jsonPath("$[0].eventId", is("event1")))
+                .andExpect(jsonPath("$[0].isAttending", is(false)))
+                .andExpect(jsonPath("$[1].userId", is("user1")))
+                .andExpect(jsonPath("$[1].eventId", is("event1")))
+                .andExpect(jsonPath("$[1].isAttending", is(true)));
+    }
+}
